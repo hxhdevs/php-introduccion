b6269326c7a962b623a2d7e4e42b96e0f3cfaed1
HXHdevs <jordisonnicolas@gmail.com>
2024-09-05 00:06:09 -0600

    Incrementos en PHP

---
+++ 6-incrementos.php
@@ -0,0 +1,13 @@
+<?php
+
+$numero1=30;
+echo 'Incremento de 1 ->'. ++$numero1;
+echo"<br/>";
+
+$numero1+=5;
+echo 'Incremento de 5 ->'.$numero1;
+echo"<br/>";
+
+$numero2 =30;
+echo 'decremento->'. --$numero2;
+?>
