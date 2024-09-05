8034810324b742393034d17a203ab594a086c391
HXHdevs <jordisonnicolas@gmail.com>
2024-09-04 23:04:17 -0600

    Tipos de datos en PHP

---
+++ 3-tipos-dato.php
@@ -0,0 +1,18 @@
+<?php
+//Boolean
+$logueado=true;
+var_dump($logueado);
+//Enteros
+$numero =200;
+var_dump($numero);
+//Flotantes
+$float = 3.1426;
+var_dump($float);
+//Strings
+$nombre = "Juan";
+var_dump($nombre);
+//Arrays
+$array =[7,'Siete'];
+var_dump($array);
+
+?>
