d74771ef6e826a56aeb0eedefade1057dd6136bf
HXHdevs <jordisonnicolas@gmail.com>
2024-09-04 23:18:05 -0600

    Operadores en PHP

---
+++ 4-operadores.php
@@ -0,0 +1,25 @@
+<?php
+
+$numero1 =20;
+$numero2 =10;
+
+//Sumar
+echo $numero1+$numero2;
+
+//Resta
+echo $numero1-$numero2;
+
+echo "<br>";
+
+//Multiplicar
+echo $numero1+$numero2;
+//Dividir
+echo $numero1/$numero2;
+
+echo "<br>";
+//Multiplicar cierta cantidad de veces
+echo 2 ** 3;
+
+
+
+?>
