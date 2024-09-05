d99484b6579c7c1eb3a6e4802af48aec9b07d772
HXHdevs <jordisonnicolas@gmail.com>
2024-09-04 22:35:18 -0600

    Variables y constantes

---
+++ 2-variables.php
@@ -0,0 +1,16 @@
+<?php 
+
+$nombre = "Hector";//Declaramos una variable
+$nombre = "Elih";//Reasignamos valor de una variable
+echo $nombre;//imprimimos el valor de una variable
+var_dump($nombre);//
+define('constante','Este es el valor de la constante');//asi se declara una constante
+echo constante; //asi se imprime una constante
+
+const constante2 = "Hola 2"; // otra forma de declarar una constante
+echo constante2;//imprimiendo constante
+
+$nombreCliente = "Pedro";
+$nombre_cliente = "Pedro";
+
+?>
