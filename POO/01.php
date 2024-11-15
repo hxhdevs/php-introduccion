18e116e31aebb3661c8a135738224097a6c4ae4d
HXHdevs <jordisonnicolas@gmail.com>
2024-11-15 00:57:01 -0600

    Metodo de clase

--- POO/01.php
+++ POO/01.php
@@ -8,12 +8,22 @@ class Producto {
     {
     }
     
+    public function mostrarProducto(){
+        echo "El producto es: ".$this->nombre.' y su precio es '.$this->precio;//usamos this para apuntar a lass variables dentro de la clase
+    }
 }
 
 
 $producto = new Producto('Monitor',200,300);
+echo "El producto es: ".$producto->nombre.' y su precio es '.$producto->precio;//Forma incorrecta accediendo a las variables publicas
+echo "<hr>";
+$producto->mostrarProducto();//Forma de obtener a travez de metodo
 dep($producto);
-
+echo "<hr>";
+echo "<hr>";
 $producto2 = new Producto('Tablet',100,500);
+echo "El producto es: ".$producto2->nombre.' y su precio es '.$producto2->precio;//Forma incorrecta accediendo a las variables publicas
+echo "<hr>";
+$producto2->mostrarProducto();//Forma de obtener a travez de metodo
 dep($producto2);
 
